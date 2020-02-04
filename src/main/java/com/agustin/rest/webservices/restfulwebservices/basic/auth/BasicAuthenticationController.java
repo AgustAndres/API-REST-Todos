b3dc5993ed5085c25bab5307c4754eb32750b79f
AgustAndres <agus_9955@hotmail.com>
2020-02-03 23:44:23 -0300

    first commit

---
+++ src/main/java/com/agustin/rest/webservices/restfulwebservices/basic/auth/BasicAuthenticationController.java
@@ -0,0 +1,12 @@
+package com.agustin.rest.webservices.restfulwebservices.basic.auth;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class BasicAuthenticationController {	
+	@GetMapping( path = "/basicauth")
+	public AuthenticationBean helloWorldBean() {
+		return new AuthenticationBean("You are authenticated");//Returns a bean converted to JSON
+	}
+}
